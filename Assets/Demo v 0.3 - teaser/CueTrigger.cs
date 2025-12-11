ec69d709a0887cba8cccbe21259c708eede6e459
Anton Filatov <anton.s.filatov@gmail.com>
2025-12-11 11:39:27 +0000

    cue1 trigger

--- Assets/Demo v 0.3 - teaser/CueTrigger.cs
+++ Assets/Demo v 0.3 - teaser/CueTrigger.cs
@@ -5,9 +5,13 @@ public class CueTrigger : MonoBehaviour
 {
     public CueController controller;
     public int cueNumber;
+    public bool shouldHideTriggerObject;
 
     private void OnTriggerEnter(Collider other)
     {
         controller.TriggerCue(cueNumber);
+        if (shouldHideTriggerObject) {
+            gameObject.SetActive(false);
+        }
     }
 }
